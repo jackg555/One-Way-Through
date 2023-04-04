33cb534ed4eaa06cf1981241f1dcd536952d18ff
jackg555 <270169808@yoobeestudent.ac.nz>
2023-04-04 13:31:56 +1200

    Couple Small Changes
    Basically same as previous version

--- One Way Through/Assets/Scripts/ballOffMap.cs
+++ One Way Through/Assets/Scripts/ballOffMap.cs
@@ -15,7 +15,7 @@ public class ballOffMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.y < respawnHeight)
+        if (transform.position.y < respawnHeight)
         {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
