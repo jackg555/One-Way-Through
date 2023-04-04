6e1bc16cff6814ebfe13565f1f6372281453eb45
jackg555 <270169808@yoobeestudent.ac.nz>
2023-04-04 13:26:46 +1200

    Added Ball Respawn
    If the ball falls off the map the ball respawns to its original position

---
+++ One Way Through/Assets/Scripts/ballOffMap.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballOffMap : MonoBehaviour
+{
+    public int respawnHeight = -5;
+    Vector3 orginalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orginalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < respawnHeight)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            transform.position = orginalPosition;
+        }
+    }
+}
